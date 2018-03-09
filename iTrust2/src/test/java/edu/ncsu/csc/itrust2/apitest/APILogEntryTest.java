7dfa2023816b4753b7e0cfcb4dd45f2d47807718
Kai Presler-Marshall <kpresle@ncsu.edu>
2018-03-08 21:51:02 -0500

    More LogAPI tests

--- iTrust2/src/test/java/edu/ncsu/csc/itrust2/apitest/APILogEntryTest.java
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/apitest/APILogEntryTest.java
@@ -1,9 +1,10 @@
 package edu.ncsu.csc.itrust2.apitest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,7 +23,10 @@ import com.google.gson.Gson;
 
 import edu.ncsu.csc.itrust2.config.RootConfiguration;
 import edu.ncsu.csc.itrust2.controllers.api.comm.LogEntryRequestBody;
+import edu.ncsu.csc.itrust2.models.enums.Role;
+import edu.ncsu.csc.itrust2.models.enums.TransactionType;
 import edu.ncsu.csc.itrust2.models.persistent.LogEntry;
+import edu.ncsu.csc.itrust2.models.persistent.User;
 import edu.ncsu.csc.itrust2.mvc.config.WebMvcConfiguration;
 
 /**
@@ -37,7 +41,7 @@ import edu.ncsu.csc.itrust2.mvc.config.WebMvcConfiguration;
 public class APILogEntryTest {
 
     private MockMvc               mvc;
-    private Gson gson = new Gson();
+    private final Gson            gson = new Gson();
     @Autowired
     private WebApplicationContext context;
 
@@ -48,24 +52,56 @@ public class APILogEntryTest {
     public void setup () {
         mvc = MockMvcBuilders.webAppContextSetup( context ).build();
     }
-    
+
     /**
      * Test for getLogByDate
      */
     @WithMockUser ( username = "admin", roles = { "USER", "ADMIN" } )
     @Test
-    public void testLogByDate() throws Exception{
-    	LogEntryRequestBody temp = new LogEntryRequestBody();
-    	temp.setStartDate("01/01/2017");
-    	temp.setEndDate("12/31/2018");
-    	temp.setPage(1);
-    	temp.setPageLength(10);
-    	
-    	
-    	mvc.perform( post("/api/v1/logentries/range")
-    				.content(gson.toJson(temp))
-    				.contentType(MediaType.APPLICATION_JSON))
-    						.andExpect( status().isOk() );
+    public void testLogByDate () throws Exception {
+        final LogEntryRequestBody temp = new LogEntryRequestBody();
+        temp.setStartDate( "01/01/2017" );
+        temp.setEndDate( "12/31/2018" );
+        temp.setPage( 1 );
+        temp.setPageLength( 10 );
+
+        mvc.perform( post( "/api/v1/logentries/range" ).content( gson.toJson( temp ) )
+                .contentType( MediaType.APPLICATION_JSON ) ).andExpect( status().isOk() );
+    }
+
+    @WithMockUser ( username = "logapitest", roles = { "USER", "ADMIN" } )
+    @Test
+    public void testLogByDateLarge () throws Exception {
+
+        final User user = new User( "logapitest", "$2a$10$EblZqNptyYvcLm/VwDCVAuBjzZOI7khzdyGPBr08PpIi0na624b8.",
+                Role.ROLE_PATIENT, 1 );
+        user.save();
+
+        for ( Integer i = 0; i < 500; ++i ) {
+            final LogEntry le = new LogEntry();
+            le.setLogCode( TransactionType.USER_BANNED );
+            le.setPrimaryUser( "logapitest" );
+            le.setTime( Calendar.getInstance() );
+
+            le.save();
+        }
+
+        final SimpleDateFormat sdf = new SimpleDateFormat( "MM/dd/yyyy" );
+
+        final Calendar start = Calendar.getInstance();
+        start.set( Calendar.MONTH, -6 );
+
+        final Calendar end = Calendar.getInstance();
+        end.set( Calendar.MONTH, 6 );
+
+        final LogEntryRequestBody temp = new LogEntryRequestBody();
+        temp.setStartDate( sdf.format( start.getTime() ) );
+        temp.setEndDate( sdf.format( end.getTime() ) );
+        temp.setPage( 6 );
+        temp.setPageLength( 10 );
+
+        mvc.perform( post( "/api/v1/logentries/range" ).content( gson.toJson( temp ) )
+                .contentType( MediaType.APPLICATION_JSON ) ).andExpect( status().isOk() );
     }
 
 }
